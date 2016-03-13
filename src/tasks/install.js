fb9e68ba7e432339ad81d32e8f3cf791480b757a
mikhael <mikhael@milikhin.name>
2016-03-13 14:06:58 +0600

    update client install script

--- src/tasks/install.js
+++ src/tasks/install.js
@@ -16,6 +16,6 @@ module.exports = function (options) {
 		'mkdir -p ./bower_components',
 		'git clone https://github.com/milikhin/r5m-client.git bower_components/r5m-cms',
 		'cd bower_components/r5m-cms; dir; git checkout ' + tagString + ' -b working; \
-			./node_modules/.bin/bower install'
+			../../node_modules/.bin/bower install'
 	]);
 };
